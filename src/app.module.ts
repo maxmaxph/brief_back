5dabbaaea1b46afba8fd5c4d9b3f8ccb3a885ad3
maxmaxph <maxmaxph@gmail.com>
2023-10-30 11:04:01 +0100

    1 creations des ressources, install typeorm, pg, prefixe et .env

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
+import { User } from './users/entities/user.entity';
+import { Category } from './categories/entities/category.entity';
+
+@Module({
+  imports: [
+    ProductsModule,
+    UsersModule,
+    CategoriesModule,
+    ConfigModule.forRoot({ envFilePath: [`.env`] }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [User, Product, Category],
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
